6cd24d18a2efa8d8482be6b09de019184d62530a
Oleg Zakladnyi <zakladniy@gmail.com>
2020-12-12 21:04:50 +0200

    feat: add repeat button

--- App/Components/Button/Button.js
+++ App/Components/Button/Button.js
@@ -1,6 +1,10 @@
 export default function button() {
     const btns = document.createElement('div');
     btns.className = 'btns';
-    btns.innerHTML = '<div class = "btn none">Start game</div>';
+    const btn = document.createElement('div');
+    btn.className = 'btn none';
+    btn.textContent = 'Start game';
+    btn.addEventListener('click', () => btn.classList.add('repeat'));
+    btns.append(btn);
     return btns
 }
